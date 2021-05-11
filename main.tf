45de3e0f9c7018558a37dc2dd3f5960eb8d8e9c4
zm-intern06 <zm-intern06@zmwarelab.com>
2021-05-11 11:50:39 +0200

    test v2

--- main.tf
+++ main.tf
@@ -2,6 +2,10 @@ provider "azurerm" {
   version = ">=2.0"
   # The "feature" block is required for AzureRM provider 2.x.
   features {}
+  subscription_id = "25a98a18-5e94-4b21-9d17-e8cf45bfd81f"
+  client_id       = "98402f85-e4a1-4d93-b58e-999590ee64bd"
+  client_secret   = "4byUy4g~-9OKqqAbi3Mt_c.t4Li1MF_yi."
+  tenant_id       = "558506eb-9459-4ef3-b920-ad55c555e729"
 }
 
 terraform {
